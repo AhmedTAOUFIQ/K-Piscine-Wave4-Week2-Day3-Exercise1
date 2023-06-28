cbda741378c27865bab4c1981e719b96ac487e66
Ahmed TAOUFIQ <ahmed.taoufiq.pro@gmail.com>
2023-06-28 14:38:08 +0200

    Injection through XML File

--- springFrameworkDiscovery/src/main/java/springFrameworkDiscovery/springFrameworkDiscovery.java
+++ springFrameworkDiscovery/src/main/java/springFrameworkDiscovery/springFrameworkDiscovery.java
@@ -1,6 +1,8 @@
 package springFrameworkDiscovery;
 
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import springFrameworkDiscovery.services.HelloWorldMessageProvider;
 import springFrameworkDiscovery.services.MessageProvider;
 import springFrameworkDiscovery.services.MessageRenderer;
@@ -9,9 +11,9 @@ import springFrameworkDiscovery.services.StandardOutMessageRenderer;
 public class springFrameworkDiscovery {
 
     public static void main(String[] args) {
-        MessageRenderer mr = new StandardOutMessageRenderer();
-        MessageProvider mp = new HelloWorldMessageProvider();
-        mr.setMessageProvider(mp);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext
+                ("spring/app-context.xml");
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
         mr.render();
     }
 
